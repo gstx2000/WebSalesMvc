33676859212427b9a0b9467d82465e05ec292cf7
Gabriel <gst203002@gmail.com>
2023-10-14 14:26:24 -0300

    Seller create form working

--- WebSalesMvc/Controllers/SellersController.cs
+++ WebSalesMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebSalesMvc.Models;
 using WebSalesMvc.Services;
 
 namespace WebSalesMvc.Controllers
@@ -16,5 +17,20 @@ namespace WebSalesMvc.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }

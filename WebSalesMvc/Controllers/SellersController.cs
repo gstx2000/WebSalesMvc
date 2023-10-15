afec919dcf815cbf21e232b9526b529219e49de1
Gabriel <gst203002@gmail.com>
2023-10-15 09:52:07 -0300

    Delete seller implemented

--- WebSalesMvc/Controllers/SellersController.cs
+++ WebSalesMvc/Controllers/SellersController.cs
@@ -12,7 +12,7 @@ namespace WebSalesMvc.Controllers
         public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
-            _departmentService = departmentService; 
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -24,16 +24,41 @@ namespace WebSalesMvc.Controllers
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments };  
+            var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindbyId(id.Value);
+
+                if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+
         }
 
 

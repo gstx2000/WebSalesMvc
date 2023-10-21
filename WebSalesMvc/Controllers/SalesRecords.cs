5851b92686a06a507f5bd76128212a87eedd2947
Gabriel <gst203002@gmail.com>
2023-10-21 11:04:09 -0300

    Grouping search and simple search form created

---
+++ WebSalesMvc/Controllers/SalesRecords.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSalesMvc.Controllers
+{
+    public class SalesRecords : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
